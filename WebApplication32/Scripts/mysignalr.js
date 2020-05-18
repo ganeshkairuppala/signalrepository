b65a50e43b114a4fe719802d20a4fd52b3b84dec
ganeshkairuppala <ganesh.kairuppala@gmail.com>
2020-05-18 16:40:43 +0530

    update upto 5182020

---
+++ WebApplication32/Scripts/mysignalr.js
@@ -0,0 +1,8 @@
+﻿$.connection.hub.start().done(function () {
+    console.log('hello');
+    $.connection.myHub.server.anounce('hi gani');
+}).fail(function () { console.log('con failed'); });
+
+$.connection.myHub.client.anounce = function (msg) { alert(msg); };
+
+(function () { alert('hi');})();
---
+++ WebApplication32/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication32.Startup))]
+namespace WebApplication32
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            ConfigureAuth(app);
+        }
+    }
+}
